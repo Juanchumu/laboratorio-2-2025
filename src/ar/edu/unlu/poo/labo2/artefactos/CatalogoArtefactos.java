bb7c0966e7e7f4512f047b0916570f9f07e15da8
Juanchumu <ricard9045@gmail.com>
2025-10-14 20:06:18 -0300

    lo que falta

--- src/ar/edu/unlu/poo/labo2/artefactos/CatalogoArtefactos.java
+++ src/ar/edu/unlu/poo/labo2/artefactos/CatalogoArtefactos.java
@@ -27,3 +27,42 @@ public class CatalogoArtefactos {
 		}
 	}
 
+
+//parte vieja
+  Set<Artefacto> conjuntoDeArtefactos = new HashSet<>();
+
+  public void agregarArtefacto(Artefacto unArtefactoNuevo) {
+    conjuntoDeArtefactos.add(unArtefactoNuevo);
+  }
+
+  public Set<Artefacto> obtenerArtefactosUnicos() {
+    return conjuntoDeArtefactos;
+  }
+
+  public List<Artefacto> buscarArtefactosPorTipo(String unTipo) {
+    List<Artefacto> listaPorTipo =  new ArrayList<>();
+    for (Artefacto artefacto : conjuntoDeArtefactos) {
+      if (artefacto.getTipo().equals(unTipo)) {
+        listaPorTipo.add(artefacto);
+      }
+    }
+    listaPorTipo.sort((arti1, arti2) -> Integer.compare(arti2.getPoder(), arti1.getPoder()));
+    return listaPorTipo;
+  }
+
+  public Map<String, Integer> contarArtefactosPorTipo() {
+    Map<String, Integer> cantidadArtefactosPorTipo = new HashMap<>();
+    for (Artefacto artefacto : conjuntoDeArtefactos) {
+      cantidadArtefactosPorTipo.merge(artefacto.getTipo(), 1, Integer::sum);
+    }
+    return cantidadArtefactosPorTipo;
+  }
+
+  public Artefacto obtenerArtefactoMasPoderoso() {
+    if (conjuntoDeArtefactos == null || conjuntoDeArtefactos.isEmpty()) {
+      return null;
+    }
+    return Collections.max(conjuntoDeArtefactos, Comparator.comparing(Artefacto::getPoder));
+  }
+}
+
